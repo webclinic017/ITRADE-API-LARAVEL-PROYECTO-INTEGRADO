21540acfcc07a08b5f0c9f58cb4ef7cfe14ffe87
MTXDevp <MTXDevp@hotmail.com>
2019-06-14 17:32:11 +0200

    building18

---
+++ ITRADE-API/resources/python/main.py
@@ -0,0 +1,30 @@
+#ARTICULOS DE INTERES
+
+#https://scholarworks.sjsu.edu/cgi/viewcontent.cgi?referer=https://www.google.com/&httpsredir=1&article=1639&context=etd_projects
+
+#https://towardsdatascience.com/stock-prediction-in-python-b66555171a2
+
+#SENTIMENTAL ANALISIS
+#https://www.aclweb.org/anthology/W18-3102
+
+
+from stocker import Stocker
+#from xdss import  Stocker
+
+
+empresa = Stocker('MSFT')
+
+empresa.plot_stock()
+
+model, model_data = empresa.create_prophet_model(days=360)
+
+""""
+
+amazon = Stocker('MSFT')
+
+amazon.plot_stock()
+
+# predict days into the future
+model, model_data = amazon.create_prophet_model(days=360)
+
+"""
